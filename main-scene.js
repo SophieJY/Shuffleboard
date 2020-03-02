141b4e45e030a9c593d5b1bde41fb9881fe85b03
Ali Mirabzadeh <ali.mz758@gmail.com>
2020-03-01 18:17:53 -0800

    made 3d Scene

--- main-scene.js
+++ main-scene.js
@@ -1,65 +1,26 @@
-window.Surface = window.classes.Surface =
-    class Surface extends Shape              // A square, demonstrating two triangles that share vertices.  On any planar surface, the interior
-        // edges don't make any important seams.  In these cases there's no reason not to re-use data of
-    {                                       // the common vertices between triangles.  This makes all the vertex arrays (position, normals,
-        constructor()                         // etc) smaller and more cache friendly.
-        {
-            super("positions", "normals");    
-                                           // Name the values we'll define per each vertex.
-                                           //order of corners: bottm left,right and upper left, right
-            this.positions.push(...Vec.cast([-5, 0, 0], [5, 0, 0], [-5, 14, 0], [5, 14, 0]));   // Specify the 4 square corner locations.
-            this.normals.push(...Vec.cast([0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]));   // Match those up with normal vectors.
-            //this.texture_coords.push(...Vec.cast([0, 0], [1, 0], [0, 1], [1, 1]));   // Draw a square in texture coordinates too.
-            this.indices.push(0, 1, 2, 1, 3, 2);                   // Two triangles this time, indexing into four distinct vertices.
-        }
-    };
+window.Cube = window.classes.Cube =
+class Cube extends Shape
+{ constructor()
+    { super( "positions", "normals" ); // Name the values we'll define per each vertex.  They'll have positions and normals.
 
-window.Points_Rectangle = window.classes.Three_Points_Rectangle =
-    class Three_Points_Rectangle extends Shape              // A square, demonstrating two triangles that share vertices.  On any planar surface, the interior
-        // edges don't make any important seams.  In these cases there's no reason not to re-use data of
-    {                                       // the common vertices between triangles.  This makes all the vertex arrays (position, normals,
-        constructor()                         // etc) smaller and more cache friendly.
-        {
-            super("positions", "normals");   
-                                            // Name the values we'll define per each vertex.
-                                            //order of corners: bottm left,right and upper left, right
-            this.positions.push(...Vec.cast([-5, 14, 0], [5, 14, 0], [-5, 16, 0], [5, 16, 0]));   // Specify the 4 square corner locations.
-            this.normals.push(...Vec.cast([0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]));   // Match those up with normal vectors.
-            //this.texture_coords.push(...Vec.cast([0, 0], [1, 0], [0, 1], [1, 1]));   // Draw a square in texture coordinates too.
-            this.indices.push(0, 1, 2, 1, 3, 2);                   // Two triangles this time, indexing into four distinct vertices.
-        }
-    };
- 
-window.Ball = window.classes.Ball =
-    class Ball extends Shape              // A square, demonstrating two triangles that share vertices.  On any planar surface, the interior
-        // edges don't make any important seams.  In these cases there's no reason not to re-use data of
-    {                                       // the common vertices between triangles.  This makes all the vertex arrays (position, normals,
-        constructor()                         // etc) smaller and more cache friendly.
-        {
-            super("positions", "normals");   
-                                            // Name the values we'll define per each vertex.
-                                            //order of corners: bottm left,right and upper left, right
-            this.positions.push(...Vec.cast([-1, -1, 1], [1, -1, 1], [-1, 1, 1], [1, 1, 1]));   // Specify the 4 square corner locations.
-            this.normals.push(...Vec.cast([0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]));   // Match those up with normal vectors.
-            //this.texture_coords.push(...Vec.cast([0, 0], [1, 0], [0, 1], [1, 1]));   // Draw a square in texture coordinates too.
-            this.indices.push(0, 1, 2, 1, 3, 2);                   // Two triangles this time, indexing into four distinct vertices.
-        }
-    };
-window.Energy_Bar = window.classes.Energy_Bar =
-    class Energy_Bar extends Shape              // A square, demonstrating two triangles that share vertices.  On any planar surface, the interior
-        // edges don't make any important seams.  In these cases there's no reason not to re-use data of
-    {                                       // the common vertices between triangles.  This makes all the vertex arrays (position, normals,
-        constructor()                         // etc) smaller and more cache friendly.
-        {
-            super("positions", "normals");   
-                                            // Name the values we'll define per each vertex.
-                                            //order of corners: bottm left,right and upper left, right
-            this.positions.push(...Vec.cast([-20, 0, 0], [-16, 0, 0], [-20, 2, 0], [-16, 2, 0]));   // Specify the 4 square corner locations.
-            this.normals.push(...Vec.cast([0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]));   // Match those up with normal vectors.
-            //this.texture_coords.push(...Vec.cast([0, 0], [1, 0], [0, 1], [1, 1]));   // Draw a square in texture coordinates too.
-            this.indices.push(0, 1, 2, 1, 3, 2);                   // Two triangles this time, indexing into four distinct vertices.
-        }
-    };
+      // First, specify the vertex positions -- just a bunch of points that exist at the corners of an imaginary cube.
+      this.positions.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [-1,-1,1], [1,-1,1], [1,1,-1],  [-1,1,-1],  [1,1,1],  [-1,1,1],
+                                        [-1,-1,-1], [-1,-1,1], [-1,1,-1], [-1,1,1], [1,-1,1],  [1,-1,-1],  [1,1,1],  [1,1,-1],
+                                        [-1,-1,1],  [1,-1,1],  [-1,1,1],  [1,1,1], [1,-1,-1], [-1,-1,-1], [1,1,-1], [-1,1,-1] ) );
+      // Supply vectors that point away from eace face of the cube.  They should match up with the points in the above list
+      // Normal vectors are needed so the graphics engine can know if the shape is pointed at light or not, and color it accordingly.
+      this.normals.push(   ...Vec.cast( [0,-1,0], [0,-1,0], [0,-1,0], [0,-1,0], [0,1,0], [0,1,0], [0,1,0], [0,1,0], [-1,0,0], [-1,0,0],
+                                        [-1,0,0], [-1,0,0], [1,0,0],  [1,0,0],  [1,0,0], [1,0,0], [0,0,1], [0,0,1], [0,0,1],   [0,0,1],
+                                        [0,0,-1], [0,0,-1], [0,0,-1], [0,0,-1] ) );
+
+               // Those two lists, positions and normals, fully describe the "vertices".  What's the "i"th vertex?  Simply the combined
+               // data you get if you look up index "i" of both lists above -- a position and a normal vector, together.  Now let's
+               // tell it how to connect vertex entries into triangles.  Every three indices in this list makes one triangle:
+      this.indices.push( 0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+                        14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22 );
+      // It stinks to manage arrays this big.  Later we'll show code that generates these same cube vertices more automatically.
+    }
+}
 window.Angle_Stick = window.classes.Angle_Stick =
     class Angle_Stick extends Shape              // A square, demonstrating two triangles that share vertices.  On any planar surface, the interior
         // edges don't make any important seams.  In these cases there's no reason not to re-use data of
@@ -69,7 +30,7 @@ window.Angle_Stick = window.classes.Angle_Stick =
             super("positions", "normals");   
                                             // Name the values we'll define per each vertex.
                                             //order of corners: bottm left,right and upper left, right
-            this.positions.push(...Vec.cast([-0.1, 0, 1.5], [0.1, 0, 1.5], [-0.1, 4, 1.5], [0.1, 4, 1.5]));   // Specify the 4 square corner locations.
+            this.positions.push(...Vec.cast([-0.1, 0, 1.5], [0.1, 0, 1.5], [-0.1, 3, 1.5], [0.1, 3, 1.5]));   // Specify the 4 square corner locations.
             this.normals.push(...Vec.cast([0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]));   // Match those up with normal vectors.
             //this.texture_coords.push(...Vec.cast([0, 0], [1, 0], [0, 1], [1, 1]));   // Draw a square in texture coordinates too.
             this.indices.push(0, 1, 2, 1, 3, 2);                   // Two triangles this time, indexing into four distinct vertices.
@@ -85,14 +46,13 @@ window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
                 context.register_scene_component(new Movement_Controls(context, control_box.parentElement.insertCell()));
 
             const r = context.width / context.height;
-            context.globals.graphics_state.camera_transform = Mat4.translation([5, -10, -30]);  // Locate the camera here (inverted matrix).
+            context.globals.graphics_state.camera_transform = Mat4.translation([5, -10, -60]);  // Locate the camera here (inverted matrix).
             context.globals.graphics_state.projection_transform = Mat4.perspective(Math.PI / 4, r, .1, 1000);
             //Adding shapes on the screen
             const shapes = {
-                'surface': new Surface(),
-                'points': new Points_Rectangle(),
-                'ball': new Ball(),
-                'energyBar': new Energy_Bar(),
+                'surface': new Cube(),
+                'ball': new Cube(),
+                'energyBar': new Cube(),
                 'angleLine': new Angle_Stick()
             };
             //initial variables
@@ -105,7 +65,7 @@ window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
             this.restart=false;
             this.energyBar=false;
             this.initialBallPosX= 0;
-            this.initialBallPosY=0;
+            this.initialBallPosZ=14;
             this.angleStickStillness=false;
             this.scaleValue=0;
             this.angleValue=0;
@@ -140,22 +100,23 @@ window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
         initial_scene(graphics_state,model_transform){
             const surfaceColor = Color.of(213/255, 213/255, 213/255, 1), onePointColor = Color.of(7/255, 1, 15/255, 1),
                 twoPointsColor = Color.of(0, 176/255, 6/255, 1), threePointsColor = Color.of(0, 115/255, 4/255, 1);
-            //draw the rectangular surface
+            //draw the rectangular-cube with rotation of Math.PI/8 arounf x-axis  surface
+            model_transform= model_transform.times(Mat4.rotation(Math.PI/8,Vec.of(1,0,0))).times(Mat4.scale([7,1,14]))
             this.shapes.surface.draw(graphics_state, model_transform, this.plastic.override({color:surfaceColor }));
             //draw three rectangles on the surface representing points
-            model_transform = Mat4.identity();
             //draw the rectangular surface
             //for three points
             model_transform=Mat4.identity();
             //1 point
-            this.shapes.points.draw(graphics_state, model_transform, this.plastic.override({color: onePointColor}));
-            model_transform=model_transform.times(Mat4.translation([0,2,0]));
+            model_transform= model_transform.times(Mat4.rotation(Math.PI/8,Vec.of(1,0,0))).times(Mat4.scale([7,1,2])).times(Mat4.translation([0,0,-8]))
+            this.shapes.surface.draw(graphics_state, model_transform, this.plastic.override({color: onePointColor}));
+            model_transform=model_transform.times(Mat4.translation([0,0,-2]));
             //2 points
-            this.shapes.points.draw(graphics_state, model_transform, this.plastic.override({color: twoPointsColor}));
-            model_transform=model_transform.times(Mat4.translation([0,2,0]));
+            this.shapes.surface.draw(graphics_state, model_transform, this.plastic.override({color: twoPointsColor}));
+            model_transform=model_transform.times(Mat4.translation([0,0,-2]));
             //3 points
-            this.shapes.points.draw(graphics_state, model_transform, this.plastic.override({color: threePointsColor}));
-            return model_transform;
+            this.shapes.surface.draw(graphics_state, model_transform, this.plastic.override({color: threePointsColor}));
+            return model_transform
         }
         //calculating final destination
         distance_calculator_helper(speed,rotation){
@@ -178,51 +139,40 @@ window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
             var rotationAngle=0;
             //create the initial scence with the surface of the game
             model_transform = this.initial_scene( graphics_state, model_transform);
-            //------------------- DRAW  THE ANGLE STICK
-            model_transform= Mat4.identity();
+            //------------------- DRAW  THE ANGLE STICK ---------
+            // model_transform= Mat4.identity();
             //rotation 
             if(!this.angleStickStillness){
-                rotationAngle= -1 * Math.sin(angleStickTime)/2;
+                rotationAngle= -1 * Math.sin(angleStickTime)/3;
                 this.angleValue = rotationAngle;
                 this.angleStillTime = angleStickTime;
             }
+            //Fix the rotation
             else{
                 rotationAngle = this.angleValue;
-                //----------- TODO ----------
-                 //NOTE: NOT SURE HOW TO FIX THE ROTATION WHEN THE USER WANTS TO STOP IT AT A CERTAIN ANGLE
-                // SO WE CAN USE THAT ANGLE FOR TRANSLATION OF THE BALL
             }
-            model_transform= model_transform.times( Mat4.rotation( rotationAngle, Vec.of(0, 0, 1 ) ) );
+            //draw the angle stick based on the surface cordinates
+            model_transform= model_transform.times(Mat4.rotation(-Math.PI/2,Vec.of(1,0,0))).times(Mat4.translation([0,-19,0])).times(Mat4.rotation(rotationAngle,Vec.of(0,0,1)))
             this.shapes.angleLine.draw(graphics_state, model_transform, this.plastic.override({color: Color.of(1,1,0,1)}))
             //--------------- DRAW ENERGY BAR--------------
-            //------------------ TODO----------------
-            //NOTE: Only one sofar, with animation we should translate it higher two time and repeat until the user press a button 
             model_transform= Mat4.identity();
             var scaleValue=0;
             scaleValue = 3+ 3*Math.sin(energyBarTime);
             let scale = [[1,0,0,0],[0, scaleValue ,0,0],[0,0,1,0],[0,0,0,1]];
-
-            model_transform = model_transform.times(scale);
-            //this implementation uses a random number to show a energy bar no animation is being used
-            //I think we should use animation tho
+            model_transform = model_transform.times(scale).times(Mat4.translation([-20,0,0])).times(Mat4.scale([2,2,1]));
+            //Animate the the Energy Bar until the user presses 'Enter'
             let color_scale = Math.sin(energyBarTime) * 0.5;
             if(!this.energyBar){
                 this.scaleValue = scaleValue;
                 this.shapes.energyBar.draw(graphics_state, model_transform, this.plastic.override({color: Color.of(0.5 + color_scale, 0, 0.5 - color_scale, 1)}));
-                //model_transform= model_transform.times(Mat4.translation([0,3,0]))
-                // this.shapes.energyBar.draw(graphics_state, model_transform, this.plastic.override({color: Color.of(1,0,0,1)}));
-                // model_transform= model_transform.times(Mat4.translation([0,3,0]))
-                // this.shapes.energyBar.draw(graphics_state, model_transform, this.plastic.override({color: Color.of(1,0,0,1)}));
             }
-            //if the user pressed 'Energy Level'
-            //A random energy bar would be chosen
+            //Fix the Energy Bar after the user pressed the enter
             else{
                 let scale = [[1,0,0,0],[0, this.scaleValue ,0,0],[0,0,1,0],[0,0,0,1]];
-                model_transform= Mat4.identity().times(scale);
+                model_transform= Mat4.identity().times(scale).times(Mat4.translation([-20,0,0])).times(Mat4.scale([2,2,1]));
                 let still_color_scale = (this.scaleValue-3)/3 * 0.5;
 
                 this.shapes.energyBar.draw(graphics_state, model_transform, this.plastic.override({color: Color.of(0.5 + still_color_scale, 0, 0.5 - still_color_scale, 1)}));
-                //energy level is between 1,3 inclusive
                 //TODO 
                 // DEFINE A BETTER SPEED THAT MAKE A BETTER TRANSLATION FOR THE GIVEN SURFACE
                 var speed = (this.scaleValue)+1 //???
@@ -235,7 +185,8 @@ window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
             //use final destination
             //-----------------TODO---------------------------- 
             //BALL TRANSFORMATION USING THE FINAL LOCATION DOESN'T WORK NOW
-            model_transform = model_transform.times(Mat4.translation([this.initialBallPosX  + distance.x, this.initialBallPosY + distance.y,0]))
+            //the ball works based on the surface cordinates
+            model_transform = model_transform.times(Mat4.rotation(Math.PI/8,Vec.of(1,0,0))).times(Mat4.translation([this.initialBallPosX+distance.x, 2 ,this.initialBallPosZ - distance.y]))
             this.shapes.ball.draw(graphics_state, model_transform, this.plastic.override({color: Color.of(0,0,1,1)}))
             
         }
