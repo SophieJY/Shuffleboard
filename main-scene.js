a3a1ea7334e98cd84c18efdc230f9fd2b289f70e
Jiayu Zhao <jyzhao1230@g.ucla.edu>
2020-03-04 01:24:13 -0800

    Created ball class and refector code acoording to it

--- main-scene.js
+++ main-scene.js
@@ -20,7 +20,7 @@ class Cube extends Shape
                         14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22 );
       // It stinks to manage arrays this big.  Later we'll show code that generates these same cube vertices more automatically.
     }
-}
+};
 window.Angle_Stick = window.classes.Angle_Stick =
     class Angle_Stick extends Shape              // A square, demonstrating two triangles that share vertices.  On any planar surface, the interior
         // edges don't make any important seams.  In these cases there's no reason not to re-use data of
@@ -36,6 +36,7 @@ window.Angle_Stick = window.classes.Angle_Stick =
             this.indices.push(0, 1, 2, 1, 3, 2);                   // Two triangles this time, indexing into four distinct vertices.
         }
     };
+
 window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
     class Shuffle_Board_Scene extends Scene_Component {
         constructor(context, control_box) {
@@ -55,21 +56,40 @@ window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
                 'energyBar': new Cube(),
                 'angleLine': new Angle_Stick(),
             };
+
+            class Ball {
+                constructor(player1or2) {
+                    this.init_pos_vec = Vec.of(0,2,14);
+                    this.pos_vec = Vec.of(0,2,14);
+                    this.vel_vec = Vec.of(0,0,0);
+                    this.player = player1or2;
+                    this.existence = false;
+                    this.init_speed = 0;
+                    this.animationStartTime = 0;
+                }
+                init_pos_vec;
+                pos_vec;
+                vel_vec;
+                player;
+                existence;
+                init_speed;
+                animationStartTime;
+            }
             //initial variables
-            this.numberOfBalls=4;
+            this.numberOfBalls=6;
+            this.currentBall=0;
             this.friction= 0.8;
             this.length=20;
             this.width=10;
-            this.score={}
+            this.score={};
             this.restart=false;
             this.energyBarStill=false;
-            this.initialBallPosX= 0;
-            this.initialBallPosZ=14;
             this.angleStickStillness=false;
             this.scaleValue=0;
             this.angleValue=0;
-            this.animationStartTime=0;
-            this.distanceTraveled=0;
+
+            this.ballArray =
+                [new Ball(1), new Ball(2), new Ball(1), new Ball(2), new Ball(1), new Ball(2)];
             // At the beginning of our program, load one of each of these shape
             // definitions onto the GPU.  NOTE:  Only do this ONCE per shape
             // design.  Once you've told the GPU what the design of a cube is,
@@ -80,13 +100,13 @@ window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
             this.submit_shapes(context, shapes);
             this.materials={
                 ball1:      context.get_instance( Texture_Rotate ).material( Color.of(0,0,0,1), { ambient: 1, texture: context.get_instance( "assets/8ball.png", false ) } ),
-            }
+            };
             // Make some Material objects available to you:
             this.clay = context.get_instance(Phong_Shader).material(Color.of(.9, .5, .9, 1), {
                 ambient: .4,
                 diffusivity: .4
             });
-            this.white = context.get_instance(Basic_Shader).material();
+            //this.white = context.get_instance(Basic_Shader).material();
             this.plastic = this.clay.override({specularity: .6});
 
             this.lights = [new Light(Vec.of(0, 5, 5, 1), Color.of(1, .4, 1, 1), 100000)];
@@ -96,6 +116,12 @@ window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
         {
             this.key_triggered_button("Energy Level - Shoot", ["Enter"], () => this.energyBarStill = () => this.energyBarStill= !this.energyBarStill);
             this.key_triggered_button("Restart", ["R"], () => this.restart = () => this.restart= !this.restart);
+            this.key_triggered_button("Switch Player", ["s"], () => {
+                this.energyBarStill = false;
+                if (this.currentBall < this.numberOfBalls) {
+                    this.currentBall += 1;
+                }
+            });
             this.key_triggered_button("Angle Control", ["A"], () => this.angleStickStillness = () => this.angleStickStillness= !this.angleStickStillness);
         }
         //display the initial scene
@@ -103,14 +129,14 @@ window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
             const surfaceColor = Color.of(213/255, 213/255, 213/255, 1), onePointColor = Color.of(7/255, 1, 15/255, 1),
                 twoPointsColor = Color.of(0, 176/255, 6/255, 1), threePointsColor = Color.of(0, 115/255, 4/255, 1);
             //draw the rectangular-cube with rotation of Math.PI/8 arounf x-axis  surface
-            model_transform= model_transform.times(Mat4.rotation(Math.PI/8,Vec.of(1,0,0))).times(Mat4.scale([7,1,14]))
+            model_transform= model_transform.times(Mat4.rotation(Math.PI/8,Vec.of(1,0,0))).times(Mat4.scale([7,1,14]));
             this.shapes.surface.draw(graphics_state, model_transform, this.plastic.override({color:surfaceColor }));
             //draw three rectangles on the surface representing points
             //draw the rectangular surface
             //for three points
             model_transform=Mat4.identity();
             //1 point
-            model_transform= model_transform.times(Mat4.rotation(Math.PI/8,Vec.of(1,0,0))).times(Mat4.scale([7,1,2])).times(Mat4.translation([0,0,-8]))
+            model_transform= model_transform.times(Mat4.rotation(Math.PI/8,Vec.of(1,0,0))).times(Mat4.scale([7,1,2])).times(Mat4.translation([0,0,-8]));
             this.shapes.surface.draw(graphics_state, model_transform, this.plastic.override({color: onePointColor}));
             model_transform=model_transform.times(Mat4.translation([0,0,-2]));
             //2 points
@@ -121,40 +147,62 @@ window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
             return model_transform
         }
         //calculating final destination
-        distance_calculator_helper(distance,rotation){
-            let finalLocationObject = {
+        value_to_vector (distance,rotation){
+            return {
                 x:  distance * Math.cos(rotation),
                 y:  distance * Math.sin(rotation),
               };
-           return finalLocationObject;
         }
+        //update position vector
+        update_pos (graphics_state) {
+            //d = vt + 1/2at^2
+            let distanceTraveled = this.ballArray[this.currentBall].init_speed * (graphics_state.animation_time / 1000 - this.ballArray[this.currentBall].animationStartTime)
+                - 1 / 2 * this.friction * (graphics_state.animation_time / 1000 - this.ballArray[this.currentBall].animationStartTime) * (graphics_state.animation_time / 1000 - this.ballArray[this.currentBall].animationStartTime);
+
+            //calculating distance
+            //returning an object for X and Y position
+            //distance.x and distance.y
+            let distanceVector = this.value_to_vector(distanceTraveled, this.angleValue + Math.PI / 2);
+            //console.log(this.ballArray[this.currentBall].init_pos_vec[0] + distanceVector.x);
+            // console.log("x:" + distanceVector.x);
+            // console.log("y:" + distanceVector.y);
+            return Vec.of(this.ballArray[this.currentBall].init_pos_vec[0] + distanceVector.x, 2, this.ballArray[this.currentBall].init_pos_vec[2] - distanceVector.y );
+        }
+
+        //update velocity vector
+        update_vel (graphics_state) {
+            let currentSpeed = this.ballArray[this.currentBall].init_speed - this.friction * (graphics_state.animation_time/1000 - this.ballArray[this.currentBall].animationStartTime);
+            let velocityVector = this.value_to_vector(currentSpeed, this.angleValue + Math.PI / 2);
+            //console.log("x:" + velocityVector.x);
+            //console.log("y:" +velocityVector.y);
+            return Vec.of(velocityVector.x, 0, -velocityVector.y );
+        }
+
         //display objects on the screen
         display(graphics_state) {
-            const angleStickTime = this.t = graphics_state.animation_time/300;
-            const energyBarTime = this.t = graphics_state.animation_time/200;
+            const angleStickTime = graphics_state.animation_time/300;
+            const energyBarTime = graphics_state.animation_time/200;
             graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
             let model_transform =  Mat4.identity();
-            //set the 
-            var rotationAngle=0;
-            //create the initial scence with the surface of the game
+
+            let rotationAngle;
+            //create the initial scene with the surface of the game
             model_transform = this.initial_scene( graphics_state, model_transform);
             //------------------- DRAW  THE ANGLE STICK ---------
-            // model_transform= Mat4.identity();
             //rotation 
             if(!this.angleStickStillness){
                 rotationAngle= -1 * Math.sin(angleStickTime)/4;
                 this.angleValue = rotationAngle;
-            }
-            //Fix the rotation
-            else{
+            } else{
                 rotationAngle = this.angleValue;
             }
             //draw the angle stick based on the surface cordinates
-            model_transform= model_transform.times(Mat4.rotation(-Math.PI/2,Vec.of(1,0,0))).times(Mat4.translation([0,-19.5,0])).times(Mat4.rotation(rotationAngle,Vec.of(0,0,1)))
-            this.shapes.angleLine.draw(graphics_state, model_transform, this.plastic.override({color: Color.of(1,1,0,1)}))
+            model_transform= model_transform.times(Mat4.rotation(-Math.PI/2,Vec.of(1,0,0))).times(Mat4.translation([0,-19.5,0])).times(Mat4.rotation(rotationAngle,Vec.of(0,0,1)));
+            this.shapes.angleLine.draw(graphics_state, model_transform, this.plastic.override({color: Color.of(1,1,0,1)}));
+
             //--------------- DRAW ENERGY BAR--------------
             model_transform= Mat4.identity();
-            var scaleValue=0;
+            let scaleValue;
             scaleValue = 3+ 3*Math.sin(energyBarTime);
             let scale = [[1,0,0,0],[0, scaleValue ,0,0],[0,0,1,0],[0,0,0,1]];
             model_transform = model_transform.times(scale).times(Mat4.translation([-20,0,0])).times(Mat4.scale([2,2,1]));
@@ -163,7 +211,7 @@ window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
             if(!this.energyBarStill){
                 this.scaleValue = scaleValue;
                 this.shapes.energyBar.draw(graphics_state, model_transform, this.plastic.override({color: Color.of(0.5 + color_scale, 0, 0.5 - color_scale, 1)}));
-                this.animationStartTime = graphics_state.animation_time/1000;
+                this.ballArray[this.currentBall].animationStartTime = graphics_state.animation_time/1000;
             }
             //Fix the Energy Bar after the user pressed the enter
             else{
@@ -172,29 +220,45 @@ window.Shuffle_Board_Scene = window.classes.Shuffle_Board_Scene =
                 let still_color_scale = (this.scaleValue-3)/3 * 0.5;
 
                 this.shapes.energyBar.draw(graphics_state, model_transform, this.plastic.override({color: Color.of(0.5 + still_color_scale, 0, 0.5 - still_color_scale, 1)}));
-                //TODO 
-                // DEFINE A BETTER SPEED THAT MAKE A BETTER TRANSLATION FOR THE GIVEN SURFACE
-                var speed = this.scaleValue+3;
+                this.ballArray[this.currentBall].init_speed = this.scaleValue+3;
             }
-            //----------------- Draw Ball: Blue Square ----------------
-            model_transform= Mat4.identity()
-            //calculating distance. returning an object for X and Y position 
-            // distance.x and distance.y
+            //----------------- Draw Ball ----------------
 
+            //update position vector based on speed and animation start time
             //vf = vi + at
-            let timeTraveled = speed/this.friction;
-            if (graphics_state.animation_time/1000 - this.animationStartTime < timeTraveled) {
-                this.distanceTraveled = speed * (graphics_state.animation_time / 1000 - this.animationStartTime) - 1 / 2 * this.friction * (graphics_state.animation_time / 1000 - this.animationStartTime) * (graphics_state.animation_time / 1000 - this.animationStartTime);
+            let timeTraveled = this.ballArray[this.currentBall].init_speed/this.friction;
+            if (graphics_state.animation_time/1000 - this.ballArray[this.currentBall].animationStartTime < timeTraveled) {
+                this.ballArray[this.currentBall].pos_vec = this.update_pos(graphics_state);
+                //console.log(this.ballArray[this.currentBall].pos_vec);
+                this.ballArray[this.currentBall].vel_vec = this.update_vel(graphics_state);
             }
-            let distance = this.distance_calculator_helper(this.distanceTraveled, this.angleValue + Math.PI / 2);
-            //use final destination
-            //-----------------TODO----------------------------
-            //BALL TRANSFORMATION USING THE FINAL LOCATION DOESN'T WORK NOW
-            //the ball works based on the surface cordinates
-            model_transform = model_transform.times(Mat4.rotation(Math.PI / 8, Vec.of(1, 0, 0))).times(Mat4.translation([this.initialBallPosX + distance.x, 2, this.initialBallPosZ - distance.y]));
+
             if(this.energyBarStill) {
-                this.shapes.ball.draw(graphics_state, model_transform, this.materials.ball1)
+                //make the current ball visible
+                this.ballArray[this.currentBall].existence = true;
             }
+
+            //iterate through the ball array and draw each existing balls
+            for(let i = 0; i < this.numberOfBalls; i++) {
+                let curr_ball = this.ballArray[i];
+
+                //set color based on player
+                let curr_color;
+                if (curr_ball.player === 1) {
+                    curr_color = Color.of(0, 0, 1, 1);
+                } else {
+                    curr_color = Color.of(1, 0, 0, 1);
+                }
+
+                //draw the balls based on their existence
+                if (curr_ball.existence) {
+                    //console.log(curr_ball.pos_vec[0]);
+                    model_transform = Mat4.identity().times(Mat4.rotation(Math.PI / 8, Vec.of(1, 0, 0)))
+                        .times(Mat4.translation([curr_ball.pos_vec[0], curr_ball.pos_vec[1], curr_ball.pos_vec[2]]));
+                    this.shapes.ball.draw(graphics_state, model_transform, this.materials.ball1);
+                }
+            }
+
         }
     };
     class Texture_Rotate extends Phong_Shader
